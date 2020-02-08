77b5ba14369eae58672ca8ca241e757adc35aee3
Martin Torres <mctorres88@gmail.com>
2020-02-08 10:51:00 -0600

    Update job_scraper.sh

--- job_scraper.sh
+++ job_scraper.sh
@@ -14,18 +14,30 @@ mv Final_Report.txt Archive_Report.txt
 
 # Scrape Websites
 # Feature request: Have search cycle through keywords
+# Feature Request: Have better search terms
 
-curl https://airtable.com/careers > jobdata/Airtable
-curl https://about.gitlab.com/jobs/apply/ > jobdata/Gitlab
+curl -s https://airtable.com/careers > jobdata/Airtable
+curl -s https://about.gitlab.com/jobs/apply/ > jobdata/Gitlab
+curl -s https://www.linuxfoundation.org/about/careers/ > jobdata/LinuxFoundation
+curl -s https://careers.mozilla.org/listings/ > jobdata/Mozilla
+curl -s https://wikimediafoundation.org/about/jobs/ > jobdata/WikiMediaFoundation
+curl -s https://duckduckgo.com/hiring/ > jobdata/DuckDuckGo
+curl -s https://www.joyent.com/about/careers > jobdata/Joyent
+curl -s https://bandcamp.com/jobs > jobdata/Bandcamp
+#curl -s https://weworkremotely.com/remote-jobs/search?utf8=%E2%9C%93&audit=term > jobdata/WeWorkRemote-Audit
+curl -s https://automattic.com/work-with-us/#open-positions > jobdata/Automattic
+curl -s https://www.chef.io/careers/open-positions/ > jobdata/Chef.io
+#curl -s https://careers-redhat.icims.com/jobs/search?ss=1&searchLocation=12781-12827-Remote > jobdata/RedHat-TX-Remote
+curl -s https://stripe.com/jobs/search?l=remote > jobdata/Stripe
 
 # Look within folder
 cd jobdata
 
-## Audit Jobs
-echo "Beginning Report for Audit Jobs"
-grep -iRl "Audit" >> reports/Audit_Jobs.txt
-echo "\n" >> reports/Audit_Jobs.txt
-sed -i "1i Found Jobs Matching 'Audit' At:" reports/Audit_Jobs.txt
+## Auditor Jobs
+echo "Beginning Report for Auditor Jobs"
+grep -iRl "Auditor" >> reports/Auditor_Jobs.txt
+echo "\n" >> reports/Auditor_Jobs.txt
+sed -i "1i Found Jobs Matching 'Auditor' At:" reports/Auditor_Jobs.txt
 echo 
 
 ## Security Jobs
